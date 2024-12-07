b6a96482c231f8ae45dcca38805908f212557690
MarkellaEgglezou <t8210039@aueb.gr>
2024-12-07 13:57:31 +0200

    Added a "Submit Form" button in ComplaintFormActivity.kt

--- lesxi/app/src/main/java/com/example/lesxi/ComplaintFormActivity.kt
+++ lesxi/app/src/main/java/com/example/lesxi/ComplaintFormActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -74,7 +75,21 @@ fun Form() {
         )
         EditTextField(modifier = Modifier.padding(bottom =
         32.dp).fillMaxWidth())
-        Spacer(modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(120.dp))
+        Button()
+    }
+}
+
+@Composable
+fun Button(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.submit))
+        }
     }
 }
 
