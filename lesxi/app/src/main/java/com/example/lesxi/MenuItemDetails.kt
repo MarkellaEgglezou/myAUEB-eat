0b447c450502c69ab03405fe4536c8b55d008670
Maria Chrysochoou <t8210168@aueb.gr>
2024-12-21 15:00:03 +0200

    I Fixed this but then ruined it again lol

--- lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
+++ lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
@@ -1,11 +1,19 @@
 package com.example.lesxi
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,74 +21,91 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.toObject
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuItemDetailsScreen(itemID: String) {
-
-    val db = FirebaseFirestore.getInstance()
-
-
+fun MenuItemDetailsScreen(itemID: String) {    val db = FirebaseFirestore.getInstance()
     var item by remember { mutableStateOf<MenuItem?>(null) }
+    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(itemID) {
-        // Query the database for a MenuItem with the specified ID
         db.collection("menu")
-            .document(itemID) // Assuming itemId is the document ID in Firestore
+            .document(itemID) // Get the document by itemID
             .get()
-            .addOnSuccessListener { documentSnapshot ->
-                if (documentSnapshot.exists()) {
-                    // Create a MenuItem object from the fetched data
-                    item = documentSnapshot.toObject(MenuItem::class.java)
-                } else {
-                    println("Document not found")
-                }
+            .addOnSuccessListener { document ->
+                item = document.toObject(MenuItem::class.java)
+                isLoading = false
             }
             .addOnFailureListener { exception ->
-                println("Error fetching document: $exception")
+                println("Error getting document: $exception")
+                isLoading = false
             }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Top
-    ) {
-        Text(
-            text = item?.title ?: "No Title",
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-        )
-
-        item?.description?.let {
-            Text(
-                text = it,
-                fontSize = 16.sp,
+    if (isLoading) {
+        CircularProgressIndicator()
+    } else {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = Color(0xFF762525),
+                        titleContentColor = Color.White
+                    ),
+                    title = {
+                        Text(
+                            text = item?.title ?: "Loading...",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    })
+            }
+        ) {
+            Column(
                 modifier = Modifier
-                    .padding(bottom = 16.dp, top = 40.dp)
-            )
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Top
+            ) {
+                item?.let { it1 -> ItemDetails(it1) }
+            }
         }
-        Text(
-            text = "Αλλεργιογόνα",
-            fontSize = 14.sp,
-            modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-        )
-        Text(
-            text = "Αλλεργιογόνα.....",
-            fontSize = 14.sp,
-            modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
-        )
-
-
-
     }
 }
 
+@SuppressLint("InlinedApi")
+@Composable
+fun ItemDetails(item: MenuItem){
+    Spacer(modifier = Modifier.height(60.dp))
+    Text(
+        text = item.description,
+        fontSize = 16.sp,
+        modifier = Modifier
+            .padding(bottom = 16.dp, top = 40.dp)
+    )
+    Text(
+        text = "Αλλεργιογόνα",
+        fontSize = 16.sp,
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier
+            .padding(bottom = 16.dp, top = 40.dp)
+    )
+    Text(
+        text = "Αλλεργιογόνα.....",
+        fontSize = 14.sp,
+        modifier = Modifier
+            .padding(bottom = 16.dp, top = 40.dp)
+    )
+
+
+}
