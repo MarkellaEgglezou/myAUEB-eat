fe7c9e0b7e4aea536a2734b41e43f6054a2e5cb2
Maria Chrysochoou <t8210168@aueb.gr>
2024-12-22 14:03:33 +0200

    Make this work & Add an Image

--- lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
+++ lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
@@ -1,6 +1,7 @@
 package com.example.lesxi
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -21,29 +24,37 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.google.firebase.firestore.FirebaseFirestore
+import coil.compose.rememberImagePainter
 import com.google.firebase.firestore.toObject
+import com.google.firebase.inappmessaging.model.ImageData
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MenuItemDetailsScreen(itemID: String) {    val db = FirebaseFirestore.getInstance()
+fun MenuItemDetailsScreen(itemID: String) {
+    val db = FirebaseFirestore.getInstance()
     var item by remember { mutableStateOf<MenuItem?>(null) }
     var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(itemID) {
         db.collection("menu")
-            .document(itemID) // Get the document by itemID
+            .document(itemID)
             .get()
             .addOnSuccessListener { document ->
                 item = document.toObject(MenuItem::class.java)
                 isLoading = false
+                println(item)
             }
             .addOnFailureListener { exception ->
                 println("Error getting document: $exception")
@@ -64,7 +75,7 @@ fun MenuItemDetailsScreen(itemID: String) {    val db = FirebaseFirestore.getIns
                     ),
                     title = {
                         Text(
-                            text = item?.title ?: "Loading...",
+                            text = item?.title ?: "Null",
                             textAlign = TextAlign.Center,
                             modifier = Modifier.fillMaxWidth()
                         )
@@ -83,10 +94,24 @@ fun MenuItemDetailsScreen(itemID: String) {    val db = FirebaseFirestore.getIns
     }
 }
 
+data class ImageData(val imageUrl: String)
+
+
+
 @SuppressLint("InlinedApi")
 @Composable
 fun ItemDetails(item: MenuItem){
     Spacer(modifier = Modifier.height(60.dp))
+    val imageData = ImageData(item.imageUrl)
+    val painter = rememberAsyncImagePainter(imageData.imageUrl)
+    Image(
+        painter = painter,
+        contentDescription = "Mousakas",
+        modifier = Modifier
+            .height(200.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp)),
+    )
     Text(
         text = item.description,
         fontSize = 16.sp,
@@ -100,12 +125,13 @@ fun ItemDetails(item: MenuItem){
         modifier = Modifier
             .padding(bottom = 16.dp, top = 40.dp)
     )
-    Text(
-        text = "Αλλεργιογόνα.....",
-        fontSize = 14.sp,
-        modifier = Modifier
-            .padding(bottom = 16.dp, top = 40.dp)
-    )
+    if (item.allergens.isNotEmpty()) {
+        for (allergen in item.allergens) {
+            Text(text = "- $allergen")  // Display each allergen
+        }
+    } else {
+        Text(text = "Δεν περιλαμβάνονται.")
+    }
 
 
 }
