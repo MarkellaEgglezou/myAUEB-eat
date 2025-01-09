bd91aaee24879ea039d0f060ffe09189bdbe0f19
mariachrisochoou <t8210168@aueb.gr>
2025-01-09 09:41:14 +0200

    Remove unused imports

--- lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
+++ lesxi/app/src/main/java/com/example/lesxi/MenuItemDetails.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -127,11 +131,16 @@ fun ItemDetails(item: MenuItem){
     )
     if (item.allergens.isNotEmpty()) {
         for (allergen in item.allergens) {
-            Text(text = "- $allergen")  // Display each allergen
+            Text(text = "- $allergen")
         }
     } else {
         Text(text = "Δεν περιλαμβάνονται.")
     }
 
-
+    androidx.compose.material3.Button(onClick = {
+        TODO()
+    }, colors = buttonColors(Color(0xFF762525)))
+    {
+        Text("Reserve this Meal")
+    }
 }
