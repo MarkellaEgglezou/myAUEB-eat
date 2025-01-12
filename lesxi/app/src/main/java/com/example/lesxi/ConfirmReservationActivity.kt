cff8353dc2abaa287240c42b957d17f465a8ffc6
Marina Gkolfinopoulou <t8210026@aueb.gr>
2025-01-12 04:15:11 +0200

    Add reservations to db

--- lesxi/app/src/main/java/com/example/lesxi/ConfirmReservationActivity.kt
+++ lesxi/app/src/main/java/com/example/lesxi/ConfirmReservationActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lesxi
 
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -8,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -15,6 +19,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.lesxi.data.model.ReservationDetails
+import com.example.lesxi.data.model.Routes
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -77,65 +83,112 @@ fun ConfirmationScreen(
             items.forEach { item ->
                 Text(item)
             }
+
+
+            val context = LocalContext.current
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    saveReservationToFirebase(
+                        context = context,
+                        navController = navController,
+                        reservationDetails = reservationDetails,
+                        items = items
+                    )
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF762525),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text("Confirm Reservation")
+            }
         }
     }
-//        Column(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .padding(paddingValues)
-//                .padding(16.dp),
-//            verticalArrangement = Arrangement.Top,
-//            horizontalAlignment = Alignment.Start
-//        ) {
-//            Spacer(modifier = Modifier.height(16.dp))
-//
-//            Text(
-//                text = "Reservation Details",
-//                style = androidx.compose.material.MaterialTheme.typography.h6,
-//                modifier = Modifier.padding(bottom = 16.dp)
-//            )
-//
-//            Spacer(modifier = Modifier.height(48.dp))
-//            val reservationDetails = ReservationDetails(date, time, people)
-//            reservationDetails?.let {
-//                DetailRow(label = "Date:", value = it.date)
-//                Spacer(modifier = Modifier.height(48.dp))
-//                DetailRow(label = "Time:", value = it.time)
-//                Spacer(modifier = Modifier.height(48.dp))
-//                DetailRow(label = "People:", value = it.numberOfPeople)
-//                Spacer(modifier = Modifier.height(48.dp))
-////                DetailRow(label = "Table:", value = it.table)
-//            }
-//
-//            Spacer(modifier = Modifier.weight(1f))
-//
-//            Row(
-//                modifier = Modifier.fillMaxWidth(),
-//                horizontalArrangement = Arrangement.SpaceBetween
-//            ) {
-//                Button(
-//                    onClick = { TODO() },
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = Color(0xFF762525),
-//                        contentColor = Color.White
-//                    )
-//                ) {
-//                    Text("Back")
-//                }
-//
-//                Button(
-//                    onClick = { TODO() },
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = Color(0xFF762525),
-//                        contentColor = Color.White
-//                    )
-//                ) {
-//                    Text("Confirm")
-//                }
-//            }
-//        }
-//    }
+
 }
+
+fun saveReservationToFirebase(
+    context: Context,
+    navController: NavHostController,
+    reservationDetails: ReservationDetails,
+    items: List<String>
+) {
+    val db = FirebaseFirestore.getInstance()
+
+    // Prepare data to save
+    val reservationData = hashMapOf(
+        "date" to reservationDetails.date,
+        "time" to reservationDetails.time.substring(19, 27),
+        "numberOfPeople" to reservationDetails.numberOfPeople,
+        "items" to items
+    )
+
+    // Save data to Firestore
+    db.collection("Reservation")
+        .add(reservationData)
+        .addOnSuccessListener {
+
+            //updateTimeSlot(reservationDetails.date, reservationDetails.time, reservationDetails.numberOfPeople.toInt())
+
+            Toast.makeText(context, "Reservation confirmed!", Toast.LENGTH_SHORT).show()
+            //navController.popBackStack()
+            navController.navigate(Routes.menu) //it doesn't land there - app turns off
+        }
+        .addOnFailureListener { exception ->
+            // Handle error
+            println("Error saving reservation: $exception")
+        }
+}
+
+/*fun updateTimeSlot(date: String, time: String, numberOfPeople: Int) {
+    val db = FirebaseFirestore.getInstance()
+
+    // Reference the document in TimeSlots collection for the given date and time
+    val timeSlotRef = db.collection("TimeSlots")
+        .whereEqualTo("date", date)
+        .whereEqualTo("time", time)
+        .limit(1)
+
+    timeSlotRef.get().addOnSuccessListener { result ->
+        if (!result.isEmpty) {
+            // Assuming the document is found and we have the timeSlot document
+            val timeSlotDocument = result.documents[0]
+            val freeSpots = timeSlotDocument.getLong("free")?.toInt() ?: 0
+
+            Log.d("updateTimeSlot", "Found time slot: $timeSlotDocument")
+
+            if (freeSpots > 0) {
+                // Update the booked spots and available spots
+                val updatedFreeSpots = freeSpots - numberOfPeople
+
+                val updates: MutableMap<String, Any> = hashMapOf(
+                    "free" to updatedFreeSpots
+                )
+
+                // Update the document with the new spots count
+                timeSlotDocument.reference.update(updates)
+                    .addOnSuccessListener {
+                        println("TimeSlot updated successfully")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("UpdateTimeSlot", "Error updating TimeSlot: ${e.message}", e)
+                        println("Error updating TimeSlot: $e")
+                    }
+            } else {
+                println("Not enough available spots")
+            }
+        } else {
+            println("No matching time slot found")
+        }
+    }
+
+}*/
 
 @Composable
 fun DetailRow(label: String, value: String) {
