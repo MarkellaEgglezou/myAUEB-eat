fb8e63b4010ff39c98ed3e9e44159e4449e6fab8
Marina Gkolfinopoulou <t8210026@aueb.gr>
2025-01-14 23:30:04 +0200

    Move FireBase related functions to ReservationDAO.kt

--- lesxi/app/src/main/java/com/example/lesxi/view/reservation/ConfirmReservationActivity.kt
+++ lesxi/app/src/main/java/com/example/lesxi/view/reservation/ConfirmReservationActivity.kt
@@ -26,6 +26,7 @@ import androidx.navigation.NavHostController
 import com.example.lesxi.R
 import com.example.lesxi.data.model.ReservationDetails
 import com.example.lesxi.data.model.Routes
+import com.example.lesxi.data.saveReservationToFirebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FieldValue
 
@@ -144,88 +145,6 @@ fun ConfirmationScreen(
         }
     }
 
-}
-
-fun saveReservationToFirebase(
-    context: Context,
-    navController: NavHostController,
-    reservationDetails: ReservationDetails,
-    items: List<String>
-) {
-    val db = FirebaseFirestore.getInstance()
-
-    // Prepare data to save
-    val reservationData = hashMapOf(
-        "am" to reservationDetails.am,
-        "date" to reservationDetails.date,
-        "time" to reservationDetails.time.substring(19, 27),
-        "numberOfPeople" to reservationDetails.numberOfPeople,
-        "items" to items
-    )
-
-    // Save data to FireBase
-    db.collection("Reservation")
-        .add(reservationData)
-        .addOnSuccessListener {
-
-            updateOrCreateDocument("TimeSlots", reservationDetails.date, reservationDetails.time.substring(19, 27), reservationDetails.numberOfPeople.toInt())
-            Toast.makeText(context, "Reservation confirmed!", Toast.LENGTH_SHORT).show()
-            //navController.popBackStack()
-            navController.navigate(Routes.main_page)
-        }
-        .addOnFailureListener { exception ->
-            // Handle error
-            println("Error saving reservation: $exception")
-        }
-}
-
-fun updateOrCreateDocument(
-    collectionName: String,
-    date: String,
-    time: String,
-    free: Int
-) {
-    val db = FirebaseFirestore.getInstance()
-
-    db.collection(collectionName)
-        .whereEqualTo("date", date)
-        .whereEqualTo("time", time)
-        .get()
-        .addOnSuccessListener { documents ->
-            if (!documents.isEmpty) {
-                // Update the existing document(s)
-                for (document in documents) {
-                    db.collection(collectionName)
-                        .document(document.id)
-                        .update("free", FieldValue.increment(-free.toLong()))
-                        .addOnSuccessListener {
-                            println("Field updated successfully for document ID: ${document.id}")
-                        }
-                        .addOnFailureListener { e ->
-                            println("Error updating field for document ID: ${document.id}: ${e.message}")
-                        }
-                }
-            } else {
-                // No matching document, create a new one
-                val newDocument = hashMapOf(
-                    "date" to date,
-                    "time" to time,
-                    "free" to (100 - free)
-                )
-
-                db.collection(collectionName)
-                    .add(newDocument)
-                    .addOnSuccessListener { newDoc ->
-                        println("New document created with ID: ${newDoc.id}")
-                    }
-                    .addOnFailureListener { e ->
-                        println("Error creating document: ${e.message}")
-                    }
-            }
-        }
-        .addOnFailureListener { e ->
-            println("Error finding document: ${e.message}")
-        }
 }
 
 @Composable
